654a3ca4c3a667a8ed8e763a0c37c5c4c0d9330f
Anne Lißner <anne_lissner@gmx.net>
2020-03-30 19:46:50 +0200

    created a simple interface.rb WIP

---
+++ interface.rb
@@ -0,0 +1,14 @@
+#ask for user input
+
+puts 'Please add an item to your clothes.'
+
+puts "Do you want to add a top or a bottom? Type 'top' or 'bottom'."
+
+top_or_bottom_input = gets.chomp
+
+if top_or_bottom_input == 'top'
+  #push string into a hash??
+elsif top_or_bottom_input == 'bottom'
+
+end
+
